84c07b6ec213835e5af0b51c4721c34c9a82e26f
Gabriel Nieves <gabrielnieves2000@gmail.com>
2023-07-31 23:04:14 -0500

    Importing Material Design and starting app

---
+++ src/components/StyledButton.js
@@ -0,0 +1,8 @@
+import { Button } from "@mui/material";
+import styled from "@emotion/styled";
+
+export const StyledButton = styled(Button)({
+  color: "red",
+  backgroundColor: "blue",
+  width: "100%",
+});
